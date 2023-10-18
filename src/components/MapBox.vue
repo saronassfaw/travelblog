74359166e10c4024d107dbe343a71cae8f1a6380
Saron <saron.assfaw@gmail.com>
2023-10-18 18:33:23 +0200

    seond commit

--- src/components/MapBox.vue
+++ src/components/MapBox.vue
@@ -1,7 +1,5 @@
 <template>
-  <div ref="mapContainer" class="map-container">
-    
-  </div>
+  <div ref="mapContainer" class="map-container"></div>
 </template>
 
 <script>
@@ -16,34 +14,39 @@ export default {
     };
   },
   mounted() {
+
     mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_MY_API_KEY;
-    const map = new mapboxgl.Map({
-      container: this.$refs.mapContainer,
-      style: 'mapbox://styles/saronassfaww/clnkjx1u1002001pg1wb70rpe',
-      center: [138.2529, 36.2048],
-      zoom: 5
-    });
 
-    this.map = map;
 
-    this.map.on('click', (event) => {
-      const features = this.map.queryRenderedFeatures(event.point, {
-        layers: ['japan']
+    mapboxgl.on('load', () => {
+      const map = new mapboxgl.Map({
+        container: this.$refs.mapContainer,
+        style: 'mapbox://styles/saronassfaww/clnkjx1u1002001pg1wb70rpe',
+        center: [138.2529, 36.2048],
+        zoom: 5
       });
-      if (!features.length) {
-        return;
-      }
-      const feature = features[0];
-      this.feature = feature;
 
-      const popup = new mapboxgl.Popup({ offset: [0, -15] })
-        .setLngLat(this.feature.geometry.coordinates)
-        .setHTML(
-          `<h3>${this.feature.properties.title}</h3><p>${this.feature.properties.description}</p>`
-        )
-        .addTo(this.map);
+      this.map = map;
 
-      this.popup = popup;
+      this.map.on('click', (event) => {
+        const features = this.map.queryRenderedFeatures(event.point, {
+          layers: ['japan']
+        });
+        if (!features.length) {
+          return;
+        }
+        const feature = features[0];
+        this.feature = feature;
+
+        const popup = new mapboxgl.Popup({ offset: [0, -15] })
+          .setLngLat(this.feature.geometry.coordinates)
+          .setHTML(
+            `<h3>${this.feature.properties.title}</h3><p>${this.feature.properties.description}</p>`
+          )
+          .addTo(this.map);
+
+        this.popup = popup;
+      });
     });
   }
 }
@@ -60,5 +63,4 @@ export default {
 .map-container:hover {
   cursor: grab;
 }
-
 </style>
