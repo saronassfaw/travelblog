7505cbaa54e5b55ed68cebfd0fdb0b6219561785
saronassfaw <135357169+saronassfaw@users.noreply.github.com>
2024-03-20 07:33:48 +0100

    Update MapBox.vue

--- src/components/MapBox.vue
+++ src/components/MapBox.vue
@@ -13,39 +13,39 @@ export default {
       popup: null
     }
   },
-  mounted() {
-    mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_MY_API_KEY
-    console.log('API Key:', mapboxgl.accessToken)
+mounted() {
+  mapboxgl.accessToken = import.meta.env.VITE_MAPBOX_MY_API_KEY
+  console.log('API Key:', mapboxgl.accessToken)
 
-    mapboxgl.on('load', () => {
-      const map = new mapboxgl.Map({
-        container: this.$refs.mapContainer,
-        style: 'mapbox://styles/saronassfaww/clnkjx1u1002001pg1wb70rpe',
-        center: [138.2529, 36.2048],
-        zoom: 5
+  const map = new mapboxgl.Map({
+    container: this.$refs.mapContainer,
+    style: 'mapbox://styles/saronassfaww/clnkjx1u1002001pg1wb70rpe',
+    center: [138.2529, 36.2048],
+    zoom: 5
+  })
+
+  this.map = map
+
+  this.map.on('load', () => {
+    this.map.on('click', (event) => {
+      const features = this.map.queryRenderedFeatures(event.point, {
+        layers: ['japan']
       })
+      if (!features.length) {
+        return
+      }
+      const feature = features[0]
+      this.feature = feature
 
-      this.map = map
+      const popup = new mapboxgl.Popup({ offset: [0, -15] })
+        .setLngLat(this.feature.geometry.coordinates)
+        .setHTML(
+          `<h3>${this.feature.properties.title}</h3><p>${this.feature.properties.description}</p>`
+        )
+        .addTo(this.map)
 
-      this.map.on('click', (event) => {
-        const features = this.map.queryRenderedFeatures(event.point, {
-          layers: ['japan']
-        })
-        if (!features.length) {
-          return
-        }
-        const feature = features[0]
-        this.feature = feature
-
-        const popup = new mapboxgl.Popup({ offset: [0, -15] })
-          .setLngLat(this.feature.geometry.coordinates)
-          .setHTML(
-            `<h3>${this.feature.properties.title}</h3><p>${this.feature.properties.description}</p>`
-          )
-          .addTo(this.map)
-
-        this.popup = popup
-      })
+      this.popup = popup
+    })
     })
   }
 }
